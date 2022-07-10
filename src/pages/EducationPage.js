fa3d6be169fc5f29b638214fae3f1a1016467049
Meir Borowski <72465933+meirborowski@users.noreply.github.com>
2022-07-10 17:57:40 +0300

    Added cards Grid to Education Page

--- src/pages/EducationPage.js
+++ src/pages/EducationPage.js
@@ -1,14 +1,62 @@
 import React from 'react';
 import Header from '../components/Header';
 import { useTranslation } from 'react-i18next';
+import CardsGrid from '../components/CardsGrid';
 
 function EducationPage() {
     const { t } = useTranslation();
+
+    const cards = [
+        {
+            id: 1,
+            title: 'Smile & Learn',
+            subtitle: '',
+            icon: 'seal.png',
+            onClick: () => {
+                const url = "intent://clever.khanacademykids.org/#Intent;scheme=https;package=org.khankids.android;end";
+
+                if (navigator.userAgent.toLowerCase().indexOf('android') > -1) {
+                    window.location.href = url;
+                }
+                else {
+                    alert("You must be using an Android device to launch the app.");
+                }
+            }
+        },
+        {
+            id: 2,
+            title: 'Écoles local',
+            subtitle: '',
+            icon: 'bird.png',
+            onClick: () => {
+                window.open('http://192.168.100.1:8099/en/learn/#/topics/t/12535ec378184bf18c8272618ebbc91b/folders?last=HOME', '_blank');
+            }
+        },
+        {
+            id: 3,
+            title: 'Agriculture',
+            subtitle: '',
+            icon: 'bird.png',
+            onClick: () => {
+                window.open('http://192.168.100.1:8099/en/learn/#/topics/t/6365a01beeac4b659211d25261ac0e92/folders?last=HOME', '_blank');
+            }
+        },
+        {
+            id: 4,
+            title: 'Hygiene',
+            subtitle: '',
+            icon: 'bird.png',
+            onClick: () => {
+                window.open('http://192.168.100.1:8099/en/learn/#/topics/t/6365a01beeac4b659211d25261ac0e92/folders?last=HOME', '_blank');
+            }
+        }
+    ];
 
     return (
         <>
             <Header pageTitle="Education" />
             <main className="education-page">
+                <CardsGrid cards={cards} />
             </main>
         </>
     )
