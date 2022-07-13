573274208848144103e7efe3aaf03e5b213686f7
Meir Borowski <72465933+meirborowski@users.noreply.github.com>
2022-07-13 20:50:24 +0300

    Fixed Loading Page

--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -7,11 +7,11 @@ function LoadingPage() {
     const { t } = useTranslation();
     const navigate = useNavigate();
 
-    useEffect(() => {
-        setTimeout(() => {
-            navigate('/home');
-        }, 3000);
-    }, [navigate]);
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         navigate('/home');
+    //     }, 3000);
+    // }, [navigate]);
 
     return (
         <div className="loading-page">
@@ -28,6 +28,9 @@ function LoadingPage() {
                 <div className="loading-page-row">
                     <img className='loading-page-col' src="../images/logo-ITU-loader.png" alt="ITU logo" />
                     <img className='loading-page-col' src="../images/logo-ansi-loader.png" alt="ANSI logo" />
+                </div>
+                <div className="loading-page-row">
+                    <img src='../images/Logo-EN-FIVI-ANSI.png' alt="EN FIVI ANSI logo" />
                 </div>
             </div>
         </div>
