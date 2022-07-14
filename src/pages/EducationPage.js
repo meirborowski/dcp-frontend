d600b34042506b6f8d71b8364475aecbffd91423
Meir Borowski <72465933+meirborowski@users.noreply.github.com>
2022-07-14 09:34:15 +0300

    Moved Media button in education to end of list

--- src/pages/EducationPage.js
+++ src/pages/EducationPage.js
@@ -4,26 +4,19 @@ import { useTranslation } from 'react-i18next';
 import CardsGrid from '../components/CardsGrid';
 import { useNavigate } from 'react-router-dom';
 
-function EducationPage() {
+function EducationPage()
+{
     const { t } = useTranslation();
     const navigate = useNavigate();
 
     const cards = [
         {
             id: 1,
-            title: 'Media',
-            subtitle: '',
-            icon: 'media.png',
-            onClick: () => {
-                navigate('/media/education');
-            }
-        },
-        {
-            id: 2,
             title: 'Smile & Learn',
             subtitle: '',
             icon: 'seal.png',
-            onClick: () => {
+            onClick: () =>
+            {
                 // const url = "intent://clever.khanacademykids.org/#Intent;scheme=https;package=org.khankids.android;end";
 
                 // if (navigator.userAgent.toLowerCase().indexOf('android') > -1) {
@@ -35,30 +28,43 @@ function EducationPage() {
             }
         },
         {
-            id: 3,
+            id: 2,
             title: 'Écoles local',
             subtitle: '',
             icon: 'bird.png',
-            onClick: () => {
+            onClick: () =>
+            {
                 window.open('http://192.168.100.1:8099/en/learn/#/topics/t/12535ec378184bf18c8272618ebbc91b/folders?last=HOME', '_blank');
             }
         },
         {
-            id: 4,
+            id: 3,
             title: 'Agriculture',
             subtitle: '',
             icon: 'bird.png',
-            onClick: () => {
+            onClick: () =>
+            {
+                window.open('http://192.168.100.1:8099/en/learn/#/topics/t/6365a01beeac4b659211d25261ac0e92/folders?last=HOME', '_blank');
+            }
+        },
+        {
+            id: 4,
+            title: 'Hygiene',
+            subtitle: '',
+            icon: 'bird.png',
+            onClick: () =>
+            {
                 window.open('http://192.168.100.1:8099/en/learn/#/topics/t/6365a01beeac4b659211d25261ac0e92/folders?last=HOME', '_blank');
             }
         },
         {
             id: 5,
-            title: 'Hygiene',
+            title: 'Media',
             subtitle: '',
-            icon: 'bird.png',
-            onClick: () => {
-                window.open('http://192.168.100.1:8099/en/learn/#/topics/t/6365a01beeac4b659211d25261ac0e92/folders?last=HOME', '_blank');
+            icon: 'media.png',
+            onClick: () =>
+            {
+                navigate('/media/education');
             }
         }
     ];
