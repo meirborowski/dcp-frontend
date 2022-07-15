48d7cb370ad508e374d4a2573c1ecb9de6021dc7
Meir Borowski <72465933+meirborowski@users.noreply.github.com>
2022-07-15 13:47:38 +0300

    Fixed Media Navbar

--- src/pages/MediaPage.js
+++ src/pages/MediaPage.js
@@ -3,18 +3,21 @@ import Header from '../components/Header';
 import MediaNav from '../components/MediaNav';
 import { useTranslation } from 'react-i18next';
 import { Outlet } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 
 
 function MediaPage({ categories })
 {
     const { t } = useTranslation();
+    const location = useLocation();
+    const pathname = location.pathname;
 
     return (
         <>
             <Header Title={t('header_title')} pageTitle={t('media_card_title')} />
             <main className="media-page">
-                <MediaNav categories={categories} />
+                {(pathname.includes('live') || pathname.includes('films')) && <MediaNav categories={categories} />}
                 <Outlet />
             </main>
         </>
